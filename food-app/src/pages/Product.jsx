69b8f33febbdfa71f2d476303cc924858b8a174c
NikitaDymovich <nikita.dymovich@student.odisee.be>
2022-06-08 19:08:56 +0200

    foto update

--- food-app/src/pages/Product.jsx
+++ food-app/src/pages/Product.jsx
@@ -9,15 +9,27 @@ import useFetch from '../hooks/useFetch';
 export const Product = () => {
    const {categorie} = useParams();
    const [products,productError,productLoading] = useFetch(`https://raw.githubusercontent.com/Niki284/Foodkiosk/main/food-app/docs/${categorie}.json`)
-   
+   console.log(products);
   return (
     <div>
+       Product
      <ul>
-       <li>
-       <p>{products.API_FOOD_URL}</p>
-       </li>
+       
+       {productLoading && <p>Loading...</p>}
+      {productError && <p>Something went wrong...</p>}
+      {products && (
+        <>
+          <h1>Posts</h1>
+           {products["pizza's"].map((product)=> (
+             <li>{product.name}
+             <img src={product.picture} alt="" />
+             </li>
+          ))}
+          
+        </>
+      )}
+      
        </ul> 
-      Product
     </div>
   )
 }
